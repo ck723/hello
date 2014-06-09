4d7f49be9509d9e8720b233ebc0b34228d50d9a6
ck723 <charleskimx82@hotmail.com>
2014-06-08 21:40:58 -0400

    hello message

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
   echo 'hello world';
-<<<<<<< HEAD
- 
-=======
-  echo 'this is the edit';
->>>>>>> 4147a11c4276c9649279a651f565fae007c44e81
+  echo 'this is the new edit';
+  //this is the comment
 ?>
